e6cdad3aded727e1d9df93d693ff75a7332da873
dunkman32 <dunkman032@gmail.com>
2021-02-20 01:28:45 +0400

    fixed some code, added styled components, added icons

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,7 +1,48 @@
 import React, {useCallback, useEffect, useState} from 'react';
 import '../../App.css';
-import { useDispatch, useSelector } from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import {actions, selectors} from '../../modules/Main'
+import {MdRefresh} from "react-icons/md";
+import styled from "styled-components";
+
+const StyledMdRefresh = styled(MdRefresh)`
+  color: green;
+  margin-left: 1rem;
+  font-size: larger;
+`;
+const HeadDiv = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ListDiv = styled.div`
+  width: 80%;
+  margin: 0 auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: .5rem .5rem .25rem;
+  border-bottom: 1px dashed gray;
+`;
+
+const Button = styled.button`
+  padding: 0.25rem 1rem;
+  border-radius: 3px;
+  /* Color the border and text with theme.main */
+  color: ${props => props.theme.main};
+  border: 2px solid ${props => props.theme.main};
+`;
+
+const Input = styled.input.attrs(props => ({
+    type: "text",
+    size: props.size || "1rem",
+}))`  
+  padding: 0.25rem;
+  border: 2px solid palevioletred;
+`;
+
 const Index = () => {
     const dispatch = useDispatch()
     const list = useSelector(selectors.selectList)
@@ -12,8 +53,11 @@ const Index = () => {
         setRows(list.data)
     }, [list])
 
-    useEffect(() => {
+    const takeList = () => {
         dispatch(actions.get.request())
+    }
+    useEffect(() => {
+        takeList()
     }, []);
 
     const addFilm = useCallback(() => {
@@ -23,14 +67,16 @@ const Index = () => {
     return (
         <div className="App">
             <hr/>
-            <input type="text" onChange={(e) => setName(e.target.value)}/>
-            <button onClick={addFilm}> Click to Fetch</button>
+            <HeadDiv>
+                <Input onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setName(e.target.value)}/>
+                <Button onClick={addFilm}> Click to Add</Button>
+                <StyledMdRefresh onClick={takeList}/>
+            </HeadDiv>
             <hr/>
             {
-                rows?.map((r: { id: string, name: string }) => <div key={r.id}>
-                    <span>{r.id}</span> - <span>{r.name}</span>
-                    <hr/>
-                </div>)
+                rows?.map((r: { id: string, name: string }) => <ListDiv key={r.id}>
+                    <span>{r.id} {r.name && ': '}</span> <span>{r.name}</span>
+                </ListDiv>)
             }
         </div>
     );
